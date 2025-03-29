551ca0f2ee39a36e0975fc20f683a5a4ec7cf85d
TaiNguyen2108 <dinhtai2108@gmail.com>
2025-03-29 20:59:57 +0700

    TaiNguyen

--- VideoEditorProjectWPF/MainWindow.xaml.cs
+++ VideoEditorProjectWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,40 @@ namespace VideoEditorProjectWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void OpenVideo_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Video Files|*.mp4;*.avi;*.mkv;*.mov"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                VideoPlayer.Source = new Uri(openFileDialog.FileName);
+                VideoPlayer.Play();
+            }
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e) => VideoPlayer.Play();
+        private void Pause_Click(object sender, RoutedEventArgs e) => VideoPlayer.Pause();
+        private void Stop_Click(object sender, RoutedEventArgs e) => VideoPlayer.Stop();
+
+        private void Skip_Click(object sender, RoutedEventArgs e)
+        {
+            if (VideoPlayer.NaturalDuration.HasTimeSpan)
+            {
+                TimeSpan newPosition = VideoPlayer.Position + TimeSpan.FromSeconds(5);
+                VideoPlayer.Position = newPosition < VideoPlayer.NaturalDuration.TimeSpan ? newPosition : VideoPlayer.NaturalDuration.TimeSpan;
+            }
+        }
+
+        private void SpeedSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SpeedSelector.SelectedItem is ComboBoxItem selectedItem)
+            {
+                VideoPlayer.SpeedRatio = double.Parse(selectedItem.Tag.ToString());
+            }
         }
     }
 }
--- VideoEditorProjectWPF/obj/Debug/net8.0-windows/LoginWindow.g.i.cs
+++ VideoEditorProjectWPF/obj/Debug/net8.0-windows/LoginWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace VideoEditorProjectWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/VideoEditorProjectWPF;V1.0.0.0;component/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VideoEditorProjectWPF;component/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\LoginWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
